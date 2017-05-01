6f24b29f01844a01aa5b33c2577a682c416c5f51
Ruslan <zhd.ruslan@gmail.com>
2017-05-01 21:22:34 +0300

    [Listener on true button]
    Add listener on true button

--- GeoQuiz/app/src/main/java/com/zhdan/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/zhdan/geoquiz/QuizActivity.java
@@ -2,8 +2,7 @@ package com.zhdan.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
@@ -13,9 +12,18 @@ public class QuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         mTrueButton = (Button) findViewById(R.id.true_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
     }
 }

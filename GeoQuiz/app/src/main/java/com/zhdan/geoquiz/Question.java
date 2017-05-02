ed747ae36af95b831612489c4d067f1c0f6deef0
Ruslan <zhd.ruslan@gmail.com>
2017-05-02 16:45:52 +0300

    [add constructio and fields]
    add constructor and fields

--- GeoQuiz/app/src/main/java/com/zhdan/geoquiz/Question.java
+++ GeoQuiz/app/src/main/java/com/zhdan/geoquiz/Question.java
@@ -1,4 +1,11 @@
 package com.zhdan.geoquiz;
 
 public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
 }

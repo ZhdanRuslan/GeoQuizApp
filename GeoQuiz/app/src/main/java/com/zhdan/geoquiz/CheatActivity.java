dbcd49e0702557d5acc7c14a3651486b52401885
Ruslan <zhd.ruslan@gmail.com>
2017-05-09 12:36:43 +0300

    [animation]
    hide with animation show answer button

--- GeoQuiz/app/src/main/java/com/zhdan/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/zhdan/geoquiz/CheatActivity.java
@@ -1,10 +1,15 @@
 package com.zhdan.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -47,6 +52,7 @@ public class CheatActivity extends AppCompatActivity {
 
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 if (mAnswerIsTrue) {
@@ -55,6 +61,21 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                int cx = mShowAnswer.getWidth() / 2;
+                int cy = mShowAnswer.getHeight() / 2;
+                float radius = mShowAnswer.getWidth();
+                Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                anim.addListener(new AnimatorListenerAdapter() {
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mAnswerTextView.setVisibility(View.VISIBLE);
+                        mShowAnswer.setVisibility(View.INVISIBLE);
+                    }
+                });
+                anim.start();
             }
         });
     }
